88f1ea5dcfc8405b95c786b1e3b8f64b7c3b537c
YuriyShorin <ushorin.03@gmail.com>
2023-12-15 14:34:03 +0300

    Initial commit

---
+++ src/main/java/hse/coursework/socialnetworkthoughts/SocialNetworkThoughtsApplication.java
@@ -0,0 +1,13 @@
+package hse.coursework.socialnetworkthoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkThoughtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkThoughtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hse/coursework/socialnetworkthoughts/SocialNetworkThoughtsApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.coursework.socialnetworkthoughts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkThoughtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
