69d8ce11eaa83c286e5284ae65e8488d6d6dd681
Maxim447 <maximbas777@gmail.com>
2024-02-11 11:58:46 +0300

    feat: добавлен маппер для преобразования сущности Profile в ProfileDto

--- src/main/java/hse/coursework/socialnetworkthoughts/service/ProfileService.java
+++ src/main/java/hse/coursework/socialnetworkthoughts/service/ProfileService.java
@@ -1,10 +1,9 @@
 package hse.coursework.socialnetworkthoughts.service;
 
-import hse.coursework.socialnetworkthoughts.dto.PostResponse;
 import hse.coursework.socialnetworkthoughts.dto.ProfileDto;
 import hse.coursework.socialnetworkthoughts.exception.ProfileNotFoundException;
 import hse.coursework.socialnetworkthoughts.mapper.ProfileMapper;
-import hse.coursework.socialnetworkthoughts.model.Post;
+import hse.coursework.socialnetworkthoughts.mapping.ProfileMapping;
 import hse.coursework.socialnetworkthoughts.model.Profile;
 
 import hse.coursework.socialnetworkthoughts.security.model.User;
@@ -13,26 +12,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
 
     private final ProfileMapper profileMapper;
+    private final ProfileMapping profileMapping;
 
     public ResponseEntity<?> getProfile(User user) {
         Profile profile = profileMapper
                 .findByUserId(user.getId())
                 .orElseThrow(ProfileNotFoundException::new);
 
-        List<PostResponse> postResponses = new ArrayList<>();
-        for (Post post : profile.getPosts()) {
-            postResponses.add(new PostResponse(post.getId(), post.getProfileId(), post.getTheme(), post.getContent(), post.getLikes(), post.getReposts(), post.getComments(), post.getViews(), post.getAuthorId(), post.getCreatedAt(), post.getEditedAt()));
-        }
-
-        ProfileDto profileDto = new ProfileDto(profile.getId(), profile.getNickname(), profile.getStatus(), profile.getDescription(), profile.getSubscribes(), profile.getSubscribers(), postResponses);
+        ProfileDto profileDto = profileMapping.toProfileDto(profile);
         return ResponseEntity.ok(profileDto);
     }
 }
